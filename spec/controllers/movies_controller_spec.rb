f79362d6b6caf632649c4d518109367a08ac573a
bon27 <jingkai64@gmail.com>
2013-02-27 11:51:04 +0800

    add file: movies_controller_spec.rb

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MoviesController do
+   describe 'Searching TMDb' do
+      it 'should call the model method that performs TMDb search'
+      it 'should select the Search Results template for rendering'
+      it 'should make the TMDb search results available to that template'
+   end
+end
+
+
